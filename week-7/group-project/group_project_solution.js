2e4542dc64a235b370edf062c35c75bdf1e8638d
DaveSpivey <dspive1@gmail.com>
2015-12-29 14:23:19 -0800

    Add 7.8 group challenge

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,12 @@
+// Pseudocode by Dave Spivey
+
+// Create a function named 'sum' that takes an array of numbers as an argument
+// Iterate through the elements and add them all together, returning the result
+
+// Create a function named 'mean' that takes an array of numbers as an argument
+// Iterate through elements and calculate the average value, returning the average
+
+// Create a function named 'median' that takes an array of numbers as an argument
+// Sort the array numerically
+// If the number of elements is odd, return the value of the number in the center of the sorted array
+// If the number of elements is even, return the value produced by averaging the center two elements
